929eb253ae2dfba477a729f494d5c7a78ddc59f6
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-20 12:15:42 -0500

    Adding utilities for geojsons

--- src/main/java/com/vpp97/tripsrestapi/utils/GeoJsonUtils.java
+++ src/main/java/com/vpp97/tripsrestapi/utils/GeoJsonUtils.java
@@ -1,2 +1,11 @@
-package com.vpp97.tripsrestapi.utils;public class GeojsonUtils {
+package com.vpp97.tripsrestapi.utils;
+
+import com.vpp97.tripsrestapi.dtos.LocationDto;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+
+public class GeoJsonUtils {
+    public static GeoJsonPoint getGeoJsonPointFromLocationDto(LocationDto locationDto){
+        return new GeoJsonPoint(locationDto.getLatitude(), locationDto.getLongitude());
+    }
+
 }
