08df4fb54bf3d75fc2df3e104c1fc2f69e7c36bb
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-20 15:29:18 -0500

    added endpoint to handle trip updation

--- src/main/java/com/vpp97/tripsrestapi/controllers/TripController.java
+++ src/main/java/com/vpp97/tripsrestapi/controllers/TripController.java
@@ -21,6 +21,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -93,5 +94,19 @@ public class TripController {
                 .build();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(genericControllerResponse);
+    }
+
+    @PutMapping(value = "{tripId}", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update a Trip by id from TripRequest")
+    public ResponseEntity<SuccessfulControllerResponse<Trip>> update(@RequestBody @Valid TripRequest tripRequest, @PathVariable("tripId") String tripId){
+        Trip trip = this.tripService.updateTripById(tripId, tripRequest);
+        String message = "Trip was updated successfully";
+        SuccessfulControllerResponse genericControllerResponse = SuccessfulControllerResponse.<Trip>builder()
+                .message(message)
+                .data(trip)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(genericControllerResponse);
     }
 }
