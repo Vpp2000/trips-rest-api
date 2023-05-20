29359bc1bc8458974e10246ec697688b70457b03
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-20 12:16:06 -0500

    Adding service method to create Trip

--- src/main/java/com/vpp97/tripsrestapi/services/TripService.java
+++ src/main/java/com/vpp97/tripsrestapi/services/TripService.java
@@ -1,12 +1,14 @@
 package com.vpp97.tripsrestapi.services;
 
 import com.vpp97.tripsrestapi.documents.Trip;
+import com.vpp97.tripsrestapi.dtos.LocationDto;
 import com.vpp97.tripsrestapi.dtos.db.Car;
 import com.vpp97.tripsrestapi.dtos.db.City;
 import com.vpp97.tripsrestapi.dtos.db.Country;
 import com.vpp97.tripsrestapi.dtos.db.Driver;
 import com.vpp97.tripsrestapi.dtos.db.MilestonePoint;
 import com.vpp97.tripsrestapi.dtos.db.Passenger;
+import com.vpp97.tripsrestapi.dtos.requests.TripRequest;
 import com.vpp97.tripsrestapi.dtos.responses.PagedResponse;
 import com.vpp97.tripsrestapi.dtos.responses.StatisticsResponse;
 import com.vpp97.tripsrestapi.exceptions.IdNotFoundException;
@@ -18,6 +20,8 @@ import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+
+import static com.vpp97.tripsrestapi.utils.GeoJsonUtils.getGeoJsonPointFromLocationDto;
 
 @Service
 @Slf4j
@@ -51,4 +55,65 @@ public class TripService {
                 .tripsByCountry(tripsByCountry)
                 .build();
     }
+
+    public Trip createTripFromTripRequest(TripRequest tripRequest) {
+
+        GeoJsonPoint startLocation = getGeoJsonPointFromLocationDto(tripRequest.getStart().getPickupLocation());
+        MilestonePoint start = MilestonePoint.builder()
+                .date(LocalDateTime.now())
+                .pickupAddress(tripRequest.getStart().getPickupAddress())
+                .pickupLocation(startLocation)
+                .build();
+
+        GeoJsonPoint endLocation = getGeoJsonPointFromLocationDto(tripRequest.getEnd().getPickupLocation());
+        MilestonePoint end = MilestonePoint.builder()
+                .date(null)
+                .pickupAddress(tripRequest.getEnd().getPickupAddress())
+                .pickupLocation(endLocation)
+                .build();
+
+        GeoJsonPoint driverLocation = getGeoJsonPointFromLocationDto(tripRequest.getDriverLocation());
+
+        Country country = Country.builder()
+                .name(tripRequest.getCountry().getName())
+                .build();
+        City city = City.builder()
+                .name(tripRequest.getCity().getName())
+                .build();
+        Passenger passenger = Passenger.builder()
+                .firstName(tripRequest.getPassenger().getFirstName())
+                .lastName(tripRequest.getPassenger().getLastName())
+                .build();
+        Driver driver = Driver.builder()
+                .firstName(tripRequest.getDriver().getFirstName())
+                .lastName(tripRequest.getDriver().getLastName())
+                .build();
+        Car car = Car.builder()
+                .plate(tripRequest.getCar().getPlate())
+                .build();
+
+        Trip trip = Trip.builder()
+                .start(start)
+                .end(end)
+                .country(country)
+                .city(city)
+                .passenger(passenger)
+                .driver(driver)
+                .car(car)
+                .status(tripRequest.getStatus())
+                .checkCode("18956")
+                .price(tripRequest.getPrice())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(null)
+                .driverLocation(driverLocation)
+                .build();
+
+        trip = this.tripRepository.insert(trip);
+
+        return trip;
+    }
+
+
+
+
 }
