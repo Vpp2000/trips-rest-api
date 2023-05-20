c344f0cdbf278c265524f59881728d14c6a89fbb
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-20 09:52:00 -0500

    first stable state of app

---
+++ src/main/java/com/vpp97/tripsrestapi/TripsRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.vpp97.tripsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripsRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vpp97/tripsrestapi/TripsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpp97.tripsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
