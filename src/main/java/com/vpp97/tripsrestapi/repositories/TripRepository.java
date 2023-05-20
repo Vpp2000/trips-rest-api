80b254629eab5582e73cd6eea20d2ef4f5e2f675
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-20 11:28:49 -0500

    added queries to handle basic statistics

--- src/main/java/com/vpp97/tripsrestapi/repositories/TripRepository.java
+++ src/main/java/com/vpp97/tripsrestapi/repositories/TripRepository.java
@@ -4,4 +4,6 @@ import com.vpp97.tripsrestapi.documents.Trip;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TripRepository extends MongoRepository<Trip, String> {
+    long countByCity_Name(String city);
+    long countByCountry_Name(String country);
 }
