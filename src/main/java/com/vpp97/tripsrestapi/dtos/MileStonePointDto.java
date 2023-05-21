d6dc9375e8ccbf2654e9be0133be23b2ba802710
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-21 17:36:27 -0500

    Fixed data management in creation un updation

--- src/main/java/com/vpp97/tripsrestapi/dtos/MileStonePointDto.java
+++ src/main/java/com/vpp97/tripsrestapi/dtos/MileStonePointDto.java
@@ -1,5 +1,6 @@
 package com.vpp97.tripsrestapi.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,11 +9,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class MileStonePointDto {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    private LocalDateTime date;
     @NotNull
     @NotEmpty(message = "Name may not be empty")
     private String pickupAddress;
