d8f6ba18d9ad3f2de439ddbc75cc0244d8b62315
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-22 09:39:50 -0500

    Adding CORS to controller

--- src/main/java/com/vpp97/tripsrestapi/config/OpenApiConfig.java
+++ src/main/java/com/vpp97/tripsrestapi/config/OpenApiConfig.java
@@ -2,10 +2,12 @@ package com.vpp97.tripsrestapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
+        servers = {@Server(url = "/", description = "Default Server URL")},
         info = @Info(
                 title = "Trips API",
                 version = "1.0",
--- src/main/java/com/vpp97/tripsrestapi/controllers/TripController.java
+++ src/main/java/com/vpp97/tripsrestapi/controllers/TripController.java
@@ -19,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,7 @@ import java.util.Map;
 
 @Tag(name = "Trip")
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("trip")
 @RequiredArgsConstructor
 public class TripController {
